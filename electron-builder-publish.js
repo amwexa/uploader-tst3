6552069360ca989bc17a946e53a46f01149f6498
Exadoctor Support <support@exadoctor.com>
2023-01-17 16:31:42 +0000

    openssl

--- electron-builder-publish.js
+++ electron-builder-publish.js
@@ -105,7 +105,7 @@ console.log('EP_RELEASE:', process.env.EP_RELEASE);
 console.log('EP_PRE_RELEASE:', process.env.EP_PRE_RELEASE);
 console.log('CSC_LINK:', process.env.CSC_LINK);
 console.log('CIRCLE_TAG:', process.env.CIRCLE_TAG);
-
+//To be removed
 
 let gh_token = process.env.GH_TOKEN;
 console.log('gh_token value:', gh_token);
